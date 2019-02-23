46deabbf66f4e55c6ae84282e73a96161813cd3a
Eric Wilson <ericswilson94@gmail.com>
2019-02-23 01:04:00 -0800

    Removed empty data from login-jquery.js

--- public/scripts/login-jquery.js
+++ public/scripts/login-jquery.js
@@ -1,10 +1,3 @@
 $(document).ready(function() {
-  
-  $(function login() {
-    let $loginButton = $("#nav-bar .login-button");
-    $loginButton.on("click", function() {
-        window.location.href = "http://localhost:8080/login";
-    })
-});
 
 })
